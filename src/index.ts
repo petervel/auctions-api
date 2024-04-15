e25b1ed8dec2f21dfc1405a4f57992a19a1fd7b1
Peter Vel <petervel@gmail.com>
2024-04-15 20:23:34 +0200

    Back to plain mysql

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
 import dotenv from "dotenv";
+import mysql, { Connection } from "mysql2";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// import { createConnection } from "mysql";
-const prisma = new PrismaClient();
+app.use(function (req, res, next) {
+	res.locals.connection = mysql.createConnection({
+		host: process.env.DB_HOST,
+		port: parseInt(process.env.DB_PORT ?? "3306"),
+		database: process.env.DB_NAME,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+	});
+	res.locals.connection.connect();
+	next();
+});
 
-// app.use(function (req, res, next) {
-// 	res.locals.connection = createConnection({
-// 		host: process.env.DB_HOST,
-// 		port: process.env.DB_PORT,
-// 		database: process.env.DB_NAME,
-// 		user: process.env.DB_USER,
-// 		password: process.env.DB_PASSWORD,
-// 	});
-// 	res.locals.connection.connect();
-// 	next();
-// });
-
-app.get('/', async (req, res) => {
-	const events = await prisma.event.findMany();
-	res.json({ "status": 200, "error": null, "response": events });
+app.get('/', (req, res) => {
+	const connection: Connection = res.locals.connection;
+	connection.query("select name from events", (error, results, fields) => {
+		if (error) throw error;
+		res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+	});
 });
 
 app.listen(port, () => {
